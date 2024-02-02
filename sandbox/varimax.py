1eba893a62eb935e7d339890cf802cc390941806
yanagikad <euler.math2718@gmail.com>
2024-02-02 18:11:52 +0900

    Fix sandbox/py

--- sandbox/varimax.py
+++ sandbox/varimax.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def varimax(Phi, gamma = 1.0, q = 20, tol = 1e-6):
+def varimax(Phi, gamma = 1.0, max_itr = 20, tol = 1e-6):
     p,k = Phi.shape
     R = np.eye(k)
     d=0
-    for i in range(q):
+    for i in range(max_itr):
         d_old = d
         Lambda = np.dot(Phi, R)
         u,s,vh = np.linalg.svd(np.dot(Phi.T,np.asarray(Lambda)**3 - (gamma/p) * np.dot(Lambda, np.diag(np.diag(np.dot(Lambda.T,Lambda))))))
